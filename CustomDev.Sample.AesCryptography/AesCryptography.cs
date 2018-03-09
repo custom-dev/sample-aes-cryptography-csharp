f788a7059d413683e12e7c8258bffcda7c42e0e9
François DORIN <francois@custom-dev.fr>
2018-03-09 11:22:37 +0100

    First release

---
+++ CustomDev.Sample.AesCryptography/AesCryptography.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CustomDev.Sample.AesCryptography
+{
+    public class AesCryptography
+    {
+        public static byte[] GetKeyFromPassword(string password, byte[] salt)
+        {            
+            Rfc2898DeriveBytes derivator = new Rfc2898DeriveBytes(password, salt, 100);
+            return derivator.GetBytes(32);
+        }        
+
+        public static byte[] EncryptWithAes(byte[] plainContent, byte[] key)
+        {
+            if (plainContent == null || plainContent.Length == 0) { throw new ArgumentNullException("plainText"); }
+            if (key == null || key.Length == 0) { throw new ArgumentNullException("key"); }
+
+            byte[] encrypted;
+            using (Aes aes = Aes.Create())
+            using(SHA256 sha256 = SHA256.Create())
+            {
+                ICryptoTransform encryptor;
+                byte[] signature = sha256.ComputeHash(plainContent);
+
+                aes.GenerateIV();
+                aes.Mode = CipherMode.CBC;
+                aes.Key = key;
+                if (aes.IV == null || aes.IV.Length != 16)
+                {
+                    throw new Exception("Invalid initialization vector");
+                }
+
+                encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    memoryStream.Write(aes.IV, 0, aes.IV.Length);
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.WriteByte(1);
+                        cryptoStream.Write(signature, 0, signature.Length);
+                        cryptoStream.Write(plainContent, 0, plainContent.Length);
+                    }
+
+                    encrypted = memoryStream.ToArray();
+                }
+            }
+
+            return encrypted;
+        }
+
+        public static byte[] DecryptWithAes(byte[] cipherText, byte[] key)
+        {
+            if (cipherText == null || cipherText.Length == 0) { throw new ArgumentNullException("cipherText"); }
+            if (key == null || key.Length == 0) { throw new ArgumentNullException("Key"); }
+
+            byte[] plainContent = null;
+
+            using (SHA256 sha256 = SHA256.Create())
+            using (Aes aes = Aes.Create())
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                byte[] initializationVector = new byte[16];
+                ICryptoTransform decryptor;
+
+                msDecrypt.Read(initializationVector, 0, initializationVector.Length);
+                aes.Mode = CipherMode.CBC;
+                aes.Key = key;
+                aes.IV = initializationVector;
+
+                decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream outputDecrypt = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        int hashAlgorithm = cryptoStream.ReadByte();
+
+                        if (hashAlgorithm == 1)
+                        {
+                            byte[] signature = new byte[32];
+                            byte[] computedSignature;
+
+                            cryptoStream.Read(signature, 0, 32);
+                            cryptoStream.CopyTo(outputDecrypt);
+                            plainContent = outputDecrypt.ToArray();
+                            computedSignature = sha256.ComputeHash(plainContent);
+
+                            if (!CompareByteArray(computedSignature, signature))
+                            {
+                                throw new Exception("Corrupted data");
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return plainContent;
+        }
+
+        private static bool CompareByteArray(byte[] array1, byte[] array2)
+        {
+            if (array1 == array2) { return true; }
+            if (array1 == null && array2 != null) { return false; }
+            if (array1 != null && array2 == null) { return false; }
+            if (array1.Length != array2.Length) { return false; }
+            
+            for(int i = 0; i < array1.Length; ++i)
+            {
+                if (array1[i] != array2[i]) { return false; }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CustomDev.Sample.AesCryptography/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace CustomDev.Sample.AesCryptography
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AesCryptography crypto = new AesCryptography();
+            if (args.Length != 3)
+            {
+                DisplayHelp();
+                return;
+            }
+
+            string command = args[0];
+            string inputFile = args[1];
+            string outputFile = args[2];
+
+            byte[] salt = Program.KeySalt;
+            string password = Program.Password;
+            byte[] inputContent = GetContent(inputFile);
+            byte[] outputContent = null;
+
+            byte[] key = AesCryptography.GetKeyFromPassword(password, salt);
+
+            switch (command)
+            {
+                case "encrypt":
+                    {
+                        outputContent = AesCryptography.EncryptWithAes(inputContent, key);
+                        break;
+                    }
+                case "decrypt":
+                    {
+                        outputContent = AesCryptography.DecryptWithAes(inputContent, key);
+                        break;
+                    }
+                default:
+                    DisplayHelp();
+                    return;
+            }
+
+            SaveContent(outputContent, outputFile);        
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("AesCryptography");
+            Console.WriteLine("===============");
+            Console.WriteLine();
+            Console.WriteLine("Usage:");
+            Console.WriteLine("AesCryptography encrypt [input file] [output file]");
+            Console.WriteLine();            
+        }
+
+        private static void SaveContent(byte[] content, string filePath)
+        {
+            File.WriteAllBytes(filePath, content);
+        }        
+
+        private static byte[] GetContent(string filePath)
+        {
+            byte[] content;
+            using (FileStream file = File.OpenRead(filePath))
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+                content = memoryStream.ToArray();
+            }
+            
+            return content;
+        }
+
+        /// <summary>
+        /// Password used to generate the key.
+        /// 
+        /// SECURITY NOTICE
+        /// ---------------
+        /// This is a SAMPLE program.
+        /// 
+        /// For security reasons, password MUST NOT BE :
+        /// - hardcoded in program (like in this program)
+        /// - passed as a argument of a command line utility 
+        /// 
+        /// You can use <see cref="https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.protecteddata?view=netframework-4.7.1">System.Security.ProtectedData</see>
+        /// to securely store a password.
+        /// </summary>
+        private static string Password
+        {
+            get { return "1234"; }
+        }
+
+        /// <summary>
+        /// Salt used to generate the key
+        /// No special consideration about security (salt can be public)
+        /// </summary>
+        private static byte[] KeySalt
+        {
+            get { return new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 }; }
+        }       
+    }
+}
